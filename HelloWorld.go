43dcf1996eec8fa6a237a2ec7292e056a8c3dc24
Eric Wang <ericwang52@yahoo.ca>
2022-01-10 10:16:59 -0500

    added endpoints, no functionality

--- HelloWorld.go
+++ HelloWorld.go
@@ -5,8 +5,20 @@ import "github.com/gofiber/fiber/v2"
 func main() {
   app := fiber.New()
 
-  app.Get("/", func(c *fiber.Ctx) error {
-    return c.SendString("Hello, World!")
+//   app.Get("/", func(c *fiber.Ctx) error {
+//     return c.SendString("Hello, World!")
+//   })
+  app.Get("/api/todo", func(c *fiber.Ctx) error {
+     return c.SendString("Hello, World!")
+   })
+  app.Post("/api/item", func(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+  })
+  app.Delete("/api/item/:id", func(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+  })
+  app.Patch("/api/item/:id", func(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
   })
 
   app.Listen(":3000")
