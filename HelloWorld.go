172ac5b551de6387756b4169afb782328fe0019d
Eric Wang <ericwang52@yahoo.ca>
2022-01-10 11:48:13 -0500

    postgres not working, trying on different computer

--- HelloWorld.go
+++ HelloWorld.go
@@ -1,23 +1,47 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"golang.org/x/crypto/bcrypt"
+  )
+  func HashPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
+}
 
+func CheckPasswordHash(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
+}
+type User struct {
+	ID           uint
+	Name         string
+	Password 	 string
+  }
 func main() {
+	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	db, _ := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	user := User{Name: "test", Password: "test"}
+
+	db.Create(&user) // pass pointer of data to Create
+
   app := fiber.New()
 
 //   app.Get("/", func(c *fiber.Ctx) error {
 //     return c.SendString("Hello, World!")
 //   })
-  app.Get("/api/todo", func(c *fiber.Ctx) error {
+  app.Get("/api/items", func(c *fiber.Ctx) error {
      return c.SendString("Hello, World!")
    })
-  app.Post("/api/item", func(c *fiber.Ctx) error {
+  app.Post("/api/items", func(c *fiber.Ctx) error {
 	return c.SendString("Hello, World!")
   })
-  app.Delete("/api/item/:id", func(c *fiber.Ctx) error {
+  app.Delete("/api/items/:id", func(c *fiber.Ctx) error {
 	return c.SendString("Hello, World!")
   })
-  app.Patch("/api/item/:id", func(c *fiber.Ctx) error {
+  app.Patch("/api/items/:id", func(c *fiber.Ctx) error {
 	return c.SendString("Hello, World!")
   })
 
